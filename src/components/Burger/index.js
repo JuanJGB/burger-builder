cc7246a6fa36d99935cbc107b9a03dbe7cb45fc4
JuanJGB <juanbenini123@gmail.com>
2020-07-11 20:48:12 -0300

    Show ingredients array

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -3,12 +3,18 @@
   import BurgerIngredient from './BurgerIngredient';
   import * as constants from './burgerConstants';
   
-  export default function Burger({ }) {
+  export default function Burger(props) {
+    const transformedIngredients = Object.keys(props.ingredients)
+      .map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+          return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
+        })
+      })
+    console.log(transformedIngredients);
     return (
       <div className={styles.Burger}>
          <BurgerIngredient type={constants.BREAD_TOP} />
-         <BurgerIngredient type={constants.CHEESE} />
-         <BurgerIngredient type={constants.MEAT} />
+         {transformedIngredients}
          <BurgerIngredient type={constants.BREAD_BOTTOM} />
       </div>
     )
--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Aux from '../../HOC/Auxiliary';
 import Burger from '../../components/Burger';
+
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      SALAD: 1,
+      BACON: 1,
+      CHEESE: 2,
+      MEAT: 2,
+    }
+  }
   render () {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     )

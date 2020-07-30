fbdb55ee4db11e28270bfd27c1657abbb07e3006
JuanJGB <juanbenini123@gmail.com>
2020-07-29 21:05:52 -0300

    Folder structure reorganized, and using a little old lifecycles. Will be converted to React Hooks in the future

--- src/components/Burger/OrderSummary/index.js
+++ src/components/Burger/OrderSummary/index.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../HOC/Auxiliary';
 import Button from '../../../components/UI/Button';
 import styles from './index.module.css'
-export default function OrderSummary ({ ingredients, purchaseCanceled, purchaseContinued, price }) {
-  const ingredientSummary = Object.keys(ingredients)
-    .map(ingredientKey => {
-      return (
-        <li key={ingredientKey}>
-          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {ingredients[ingredientKey]}
-        </li>
-      );
-    });
-  return (
-    <div>
-      <Aux>
-        <h3>Your order</h3>
-        <p>A delicious burger with the following ingredientes</p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p className={styles.TotalPrice}>Total Price: {price.toFixed(2)}</p>
-        <p>Continue to Checkout?</p>
-        <Button buttonType="Danger" clicked={purchaseCanceled}>CANCEL</Button>
-        <Button buttonType="Success" clicked={purchaseContinued}>CONTINUE</Button>
-      </Aux>
-    </div>
-  )
+export default class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[OrderSummary] DidUpdate');
+  }
+  ingredientSummary = Object.keys(this.props.ingredients)
+  .map(ingredientKey => {
+    return (
+      <li key={ingredientKey}>
+        <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
+      </li>
+    );
+  });
+  render() {
+    return (
+      <div>
+        <Aux>
+          <h3>Your order</h3>
+          <p>A delicious burger with the following ingredientes</p>
+          <ul>
+            {this.props.ingredientSummary}
+          </ul>
+          <p className={styles.TotalPrice}>Total Price: {this.props.price.toFixed(2)}</p>
+          <p>Continue to Checkout?</p>
+          <Button buttonType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+          <Button buttonType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+      </div>
+    )
+  } 
+  
 }
--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './index.module.css';
 import Aux from '../../../HOC/Auxiliary';
 import Backdrop from '../Backdrop';
 
-export default function Modal({ children, show, modalClosed }) {
-  return (
-    <Aux>
-      <Backdrop show={show} clicked={modalClosed}/>
-      <div 
-        className={styles.Modal}
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0'
-        }}>
-        {children}
-      </div> 
-    </Aux> 
-    
-  )
-}
+export default class Modal extends Component {
+  //will be converted to functional component
+  shouldComponentUpdate(nextProps, nextState ) {
+    return nextProps.show !== this.props.show;
+  }
+  componentDidUpdate () {
+    console.log('[Modal] DidUpdate');
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div 
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div> 
+      </Aux> 
+      
+    )
+  };
+  
+  }
+  
